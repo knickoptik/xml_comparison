091844fa0aeb5019aeccdf012b505f3899118d9e
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-01 19:04:31 +0200

    - Move files to data folder.

--- compare_xml.py
+++ compare_xml.py
@@ -1,4 +1,12 @@
 import xml.etree.ElementTree as ET
+import logging
+
+
+class Document:
+    def __init__(self, form_id: str, contract_number: str, form: object):
+        self.form_id = form_id
+        self.contract_number = contract_number
+        self.form = form
 
 
 class Parser:
@@ -17,10 +25,14 @@ class Parser:
         print(ET.tostring(root, encoding='utf8').decode('utf8'))
 
 
+# todo: Only one parser instead of two parsers.
 def main():
+    logging.info('Parsing documents.')
     parser = Parser('VTB_Max_Muster_Produktion.xml')
     root = parser.get_root()
-    print(parser.find_tag(root, 'v_vertragsnummer'))
+    parser.find_tag(root, 'formular')
+    # document_production = Document()
+    # document_test = Document()
 
 
 if __name__ == "__main__":
