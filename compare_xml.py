dc228502e7f4e1ea2d9c84e64b02ab9d1a6ee06f
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-01 12:45:18 +0200

    - Initialised the tree of Produktion.xml

--- compare_xml.py
+++ compare_xml.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello World!")
+import xml.etree.ElementTree as ET
 
+def main():
+    tree = ET.parse('VTB_Max_Muster_Produktion.xml')
+    root = tree.getroot()
+    print(root)
 
 if __name__ == "__main__":
     main()
