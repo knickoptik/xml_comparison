d95d9efcc200db6a85bbd61b62b713f49c4990da
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-01 13:47:13 +0200

    - Created the Parser class.

--- compare_xml.py
+++ compare_xml.py
@@ -1,12 +1,23 @@
 import xml.etree.ElementTree as ET
 
-def main():
-    # Parse document and get root element.
-    tree = ET.parse('VTB_Max_Muster_Produktion.xml')
-    root = tree.getroot()
 
-    # View document.
-    print(ET.tostring(root, encoding='utf8').decode('utf8'))
+class Parser:
+
+    def get_root(self, document):
+        tree = ET.parse(document)
+        return tree.getroot()
+
+    def find_tag(self, root, tag_name):
+        for tag in root.find(tag_name):
+            print(tag.text)
+
+    def view_document(self, root):
+        print(ET.tostring(root, encoding='utf8').decode('utf8'))
+
+
+def main():
+    parser = Parser()
+
 
 if __name__ == "__main__":
     main()
