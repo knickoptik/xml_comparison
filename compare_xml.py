a8b31d17ba23cf0910b68f9889760ce65164f363
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-01 13:02:30 +0200

    - Parse document and get root element. - View entire document.

--- compare_xml.py
+++ compare_xml.py
@@ -1,9 +1,12 @@
 import xml.etree.ElementTree as ET
 
 def main():
+    # Parse document and get root element.
     tree = ET.parse('VTB_Max_Muster_Produktion.xml')
     root = tree.getroot()
-    print(root)
+
+    # View document.
+    print(ET.tostring(root, encoding='utf8').decode('utf8'))
 
 if __name__ == "__main__":
     main()
