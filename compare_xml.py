0ebc2717ad51d1d79a4053803f456eb4a7a4d7d5
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-02 13:24:35 +0200

    - Results are saved to a simple log file. - Log file is overwritten each time the script is run. - Logging level is set to 'debug'. - Log messages in a readable format.

--- compare_xml.py
+++ compare_xml.py
@@ -1,11 +1,11 @@
+# todo: Imports should be in a try/catch block.
 import xml.etree.ElementTree as ET
 import os
-from collections import OrderedDict
 import logging
 
 
 class Document:
-    def __init__(self, form_id: str, contract_number: str, form: OrderedDict):
+    def __init__(self, form_id: str, contract_number: str, form: object):
         self.form_id = form_id
         self.contract_number = contract_number
         self.form = form
@@ -32,18 +32,21 @@ class Parser:
 
 # todo: Delete all objects after comparison --> too much memory.
 def main():
-    logging.info('Parse documents.')
+    logging.basicConfig(
+        filename='comparison.log',
+        level=logging.DEBUG, filemode='w',
+        format='%(asctime)s %(levelname)s %(message)s',
+        datefmt='%m/%d/%Y %I:%M:%S %p'
+    )
+    logging.info('Parsing documents.')
     for file in os.listdir('data'):
         try:
             parser = Parser('data/' + file)
             root = parser.get_root()
-            form = parser.find_tag(root, 'formular')
-            form_id = str(parser.get_attribute(form).get('id'))
-            print(form_id)
+            xml = parser.find_tag(root, 'formular')
+            form_id = str(parser.get_attribute(xml).get('id'))
             contract_number = parser.find_tag(root, 'v_vertragsnummer').text
-            print(contract_number)
-            document = Document(form_id, contract_number, root)
-            print(document)
+            Document(form_id, contract_number, xml)
         except ET.ParseError as e:
             logging.warning('File ' + file + ' cannot be parsed.\n' + str(e))
 
