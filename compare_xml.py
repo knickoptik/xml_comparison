0ac20da53f8eb2af3878ae16f7e6e7b3f254725e
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-02 14:57:52 +0200

    - Raise an exception if file is not in xml - format.

--- compare_xml.py
+++ compare_xml.py
@@ -5,15 +5,16 @@ import logging
 
 
 class Document:
-    def __init__(self, form_id: str, contract_number: str, form: object):
+    def __init__(self, form_id: str, contract_number: object, form: object):
         self.form_id = form_id
         self.contract_number = contract_number
         self.form = form
 
 
 class Parser:
-    def __init__(self, file):
+    def __init__(self, file: object, documents: dict):
         self.file = file
+        self.documents = documents
 
     def get_root(self):
         tree = ET.parse(self.file)
@@ -31,6 +32,8 @@ class Parser:
 
 
 # todo: Delete all objects after comparison --> too much memory.
+# todo: Compare all tags and texts --> log mismatches.
+# todo: Log where in the hierarchy of the xml - document the error occured.
 def main():
     logging.basicConfig(
         filename='comparison.log',
@@ -38,15 +41,21 @@ def main():
         format='%(asctime)s %(levelname)s %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p'
     )
+    documents = dict()
     logging.info('Parsing documents.')
     for file in os.listdir('data'):
         try:
-            parser = Parser('data/' + file)
-            root = parser.get_root()
-            xml = parser.find_tag(root, 'formular')
-            form_id = str(parser.get_attribute(xml).get('id'))
-            contract_number = parser.find_tag(root, 'v_vertragsnummer').text
-            Document(form_id, contract_number, xml)
+            if file.endswith('.xml'):
+                parser = Parser('data/' + file, documents)
+                root = parser.get_root()
+                xml = parser.find_tag(root, 'formular')
+                form_id = str(parser.get_attribute(xml).get('id'))
+                contract_number = parser.find_tag(root, 'v_vertragsnummer')
+                # todo: Document name should be given automatically.
+                documents['file'] = Document(form_id, contract_number, xml)
+            else:
+                logging.warning('File is not in xml format.')
+                raise TypeError
         except ET.ParseError as e:
             logging.warning('File ' + file + ' cannot be parsed.\n' + str(e))
 
