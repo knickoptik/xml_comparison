7fa7c50d53f1ef049b5df87ff05e5b6745c9d37c
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-03 11:20:28 +0200

    - Unittest module logging to console and logging module logging to log file.

--- compare_xml.py
+++ compare_xml.py
@@ -1,7 +1,22 @@
 # todo: Imports should be in a try/catch block.
 import xml.etree.ElementTree as ET
 import os
+import sys
 import logging
+import unittest
+
+
+logging.basicConfig(
+    filename='compare_xml.log',
+    level=logging.DEBUG, filemode='w',
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+)
+
+logger = logging.getLogger()
+# Unittest module output should not overwrite logger output.
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
 
 
 class Document:
@@ -9,6 +24,7 @@ class Document:
         self.form_id = form_id
         self.contract_number = contract_number
         self.form = form
+        logger.debug('Created document: Form ID - {}, Contract Number - {}'.format(self.form_id, self.contract_number))
 
 
 class Parser:
@@ -31,34 +47,38 @@ class Parser:
         return ET.tostring(element, encoding='utf8').decode('utf8')
 
 
-# todo: Delete all objects after comparison --> too much memory.
+class CompareXml(unittest.TestCase):
+    def setUp(self):
+        documents = dict()
+        logger.info('Parsing documents.')
+        for file in os.listdir('data'):
+            try:
+                if file.endswith('.xml'):
+                    parser = Parser('data/' + file, documents)
+                    root = parser.get_root()
+                    xml = parser.find_tag(root, 'formular')
+                    form_id = str(parser.get_attribute(xml).get('id'))
+                    contract_number = parser.find_tag(root, 'v_vertragsnummer')
+                    # todo: Document name should be given automatically.
+                    documents[file] = Document(form_id, contract_number, xml)
+                else:
+                    logger.error('File is not in xml format.')
+                    raise TypeError
+            except ET.ParseError as e:
+                logger.error('File ' + file + ' cannot be parsed.\n' + str(e))
+
+    def test_compare(self):
+        self.assertTrue(1 == 2)
+
+
 # todo: Compare all tags and texts --> log mismatches.
 # todo: Log where in the hierarchy of the xml - document the error occured.
-def main():
-    logging.basicConfig(
-        filename='comparison.log',
-        level=logging.DEBUG, filemode='w',
-        format='%(asctime)s %(levelname)s %(message)s',
-        datefmt='%m/%d/%Y %I:%M:%S %p'
-    )
-    documents = dict()
-    logging.info('Parsing documents.')
-    for file in os.listdir('data'):
-        try:
-            if file.endswith('.xml'):
-                parser = Parser('data/' + file, documents)
-                root = parser.get_root()
-                xml = parser.find_tag(root, 'formular')
-                form_id = str(parser.get_attribute(xml).get('id'))
-                contract_number = parser.find_tag(root, 'v_vertragsnummer')
-                # todo: Document name should be given automatically.
-                documents['file'] = Document(form_id, contract_number, xml)
-            else:
-                logging.warning('File is not in xml format.')
-                raise TypeError
-        except ET.ParseError as e:
-            logging.warning('File ' + file + ' cannot be parsed.\n' + str(e))
 
 
 if __name__ == "__main__":
-    main()
+    # Log output of test runner to log file.
+    # log_file = 'comparison.log'
+    # with open(log_file, 'w') as f:
+    #     runner = unittest.TextTestRunner(f)
+    #     unittest.main(testRunner=runner)
+    unittest.main()
