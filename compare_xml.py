95d1314e20481310e506b109cb80a2a5b046e1d3
Matthias Nörr <matthias.nrr@byteagenten.de>
2020-04-04 16:01:44 +0200

    - Implemented recursive function that records the level of an element in the tree and prints the elements position.

--- compare_xml.py
+++ compare_xml.py
@@ -4,6 +4,8 @@ try:
     import sys
     import logging
     import unittest
+    import xml.sax as sax
+    import xml.sax.handler as saxhandler
 except ImportError as e:
     logging.critical('Importing dependency failed with error: ' + str(e))
 
@@ -62,6 +64,21 @@ class Parser:
         res = children[:temp]
         logger.debug('Parent nodes are: ' + str(res))
         return res
+
+    def perf_func(self, elem, func, level=0):
+        """
+        Recursive function that records the level of an element in the
+        tree and prints the elements position.
+        :param elem:
+        :param func:
+        :param level:
+        """
+        func(elem, level)
+        for child in elem.getchildren():
+            self.perf_func(child, func, level+1)
+
+    def print_level(self, elem, level):
+        logger.debug('-'*level+elem.tag)
 
 
 class CompareXml(unittest.TestCase):
@@ -122,37 +139,41 @@ class CompareXml(unittest.TestCase):
             except ET.ParseError as e:
                 logger.error('File ' + file + ' cannot be parsed.\n' + str(e))
 
-    def test_tag_differences(self):
-        children_prod = self.parser.get_children(self.get_document(0).form)
-        children_test = self.parser.get_children(self.get_document(1).form)
-        tags_prod = self.get_tags(children_prod)
-        tags_test = self.get_tags(children_test)
-        tags_prod = set(tags_prod)
-        tags_test = set(tags_test)
-        diff_prod_to_test = tags_prod.difference(tags_test)
-        diff_prod_to_test = list(diff_prod_to_test)
+    def test(self):
+        root = self.get_document(0).form
+        self.parser.perf_func(root, self.parser.print_level)
 
-        logger.debug(tags_prod.difference(tags_test))
-
-        diff_test_to_prod = tags_test.difference(tags_prod)
-        diff_test_to_prod = list(diff_test_to_prod)
-
-        logger.debug(tags_test.difference(tags_prod))
-
-        for i in range(len(diff_prod_to_test)):
-            location = self.parser.find_tag_xpath(self.get_document(0).form, diff_prod_to_test[i])
-            self.parser.get_parent_nodes(self.get_document(0).form, location)
-            # todo: Save parents list as new xml tree.
-
-    def test_text_differences(self):
-        children_prod = self.parser.get_children(self.get_document(0).form)
-        children_test = self.parser.get_children(self.get_document(1).form)
-        texts_prod = self.get_texts(children_prod)
-        texts_test = self.get_texts(children_test)
-        texts_prod = set(texts_prod)
-        texts_test = set(texts_test)
-        #logger.debug(texts_prod.difference(texts_test))
-        #logger.debug(texts_test.difference(texts_prod))
+    # def test_tag_differences(self):
+    #     children_prod = self.parser.get_children(self.get_document(0).form)
+    #     children_test = self.parser.get_children(self.get_document(1).form)
+    #     tags_prod = self.get_tags(children_prod)
+    #     tags_test = self.get_tags(children_test)
+    #     tags_prod = set(tags_prod)
+    #     tags_test = set(tags_test)
+    #     diff_prod_to_test = tags_prod.difference(tags_test)
+    #     diff_prod_to_test = list(diff_prod_to_test)
+    #
+    #     logger.debug(tags_prod.difference(tags_test))
+    #
+    #     diff_test_to_prod = tags_test.difference(tags_prod)
+    #     diff_test_to_prod = list(diff_test_to_prod)
+    #
+    #     logger.debug(tags_test.difference(tags_prod))
+    #
+    #     for i in range(len(diff_prod_to_test)):
+    #         location = self.parser.find_tag_xpath(self.get_document(0).form, diff_prod_to_test[i])
+    #         self.parser.get_parent_nodes(self.get_document(0).form, location)
+    #         # todo: Save parents list as new xml tree.
+    #
+    # def test_text_differences(self):
+    #     children_prod = self.parser.get_children(self.get_document(0).form)
+    #     children_test = self.parser.get_children(self.get_document(1).form)
+    #     texts_prod = self.get_texts(children_prod)
+    #     texts_test = self.get_texts(children_test)
+    #     texts_prod = set(texts_prod)
+    #     texts_test = set(texts_test)
+    #     #logger.debug(texts_prod.difference(texts_test))
+    #     #logger.debug(texts_test.difference(texts_prod))
 
 # todo: Compare all tags and texts -> log mismatches.
 # todo: Set into account that some tags occur more than once in the document -> check block id
